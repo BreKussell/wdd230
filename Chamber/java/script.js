d3235202e0344b28150fc9d2aa4ecebf7c249295
BreKussell <rus17008@byui.edu>
2022-02-05 19:50:15 -0700

    commit

--- Chamber/java/script.js
+++ Chamber/java/script.js
@@ -10,24 +10,30 @@ x.className = "top";
 
 
 
-// date function at top of 
-function message() {
+// date function at top of page
 // Get the input from computer's clock.
 const now = new Date();
-let month = now.getMonth();
+//full month name
+let monthName = now.toLocaleString('default', { month: 'long' })
 let dayOfMonth = now.getDate();
 let dayOfWeek = now.getDay();
 let year = now.getFullYear();
-let date = dayOfWeek + dayOfMonth + year;
-// Conditions for sleeping in. 
+//weekday name
+let weekday = now.toLocaleString("default", { weekday: "long" })
+// full date: day, month day of month, year
+let date = `${weekday}, ${monthName} ${dayOfMonth}, ${year}`;
+// Conditions for long message 
 if (
   dayOfWeek == 1 ||
   dayOfWeek == 2
 ) {
-  message = date + <br /> + " Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+  message = date + "<br />" + " Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 } else {
   message = date;
 }
 // Display message in h3 where id = date
 document.getElementById('date').innerHTML = message;
-}
+
+
+
+// Weather app
