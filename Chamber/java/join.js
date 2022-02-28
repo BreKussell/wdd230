2fd11de393026d9d842b550ceb223dad61b37405
BreKussell <rus17008@byui.edu>
2022-02-28 01:14:35 -0700

    commit

--- Chamber/java/join.js
+++ Chamber/java/join.js
@@ -28,3 +28,20 @@ function next() {
     }
   }
 
+// Submit Redirect
+  document.getElementsByClassName("submit").onclick = function () {
+    location.href = "TY.html";
+};
+
+
+// Character limitaion 
+function limit(element)
+{
+    const element = getElementById(title);
+    var max_chars = 7;
+
+    if(element.value.length > max_chars) {
+        element.value = element.value.substr(0, max_chars);
+    }
+
+}
