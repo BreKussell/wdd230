de5b33f1c0277f04b5cf4f45b1bd4177d2bf95ff
BreKussell <rus17008@byui.edu>
2022-03-11 21:48:31 -0700

    commit

--- Chamber/java/script.js
+++ Chamber/java/script.js
@@ -58,45 +58,49 @@ function change()
 
 // Wind Chill
 // fetch data
-const weatherAppInfo = "https://api.openweathermap.org/data/2.5/weather?id=5604045&appid=cca4336ed1a11c87c882d80f866b168d";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604045&appid=cca4336ed1a11c87c882d80f866b168d";
 fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
+    .then((response) => response.json())
+    .then((jsObject) => {
     console.log(jsObject);
-  const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-   const desc = jsObject.weather[0].description;
-  // Kelvins 
-  let K = jsObject.main.temp;
-  // K to F
-  let temp = 1.8*(K-273) + 32;
-  // wind
-  kph = jsObject.wind.speed;
-  let wind = kph*0.62;
-  // call windChill function
-  let calcWindChill = windChill(temp, wind);
-  // display output in h3
-  if (
-    wind <= 3 ||
-    temp >= 50
-  ) {
-    message = iconsrc + "<br />" + "Conditions: " + desc + "<br />" +  "Wind Chill:  N/A";
-  } else {
-    message = iconsrc + "<br />" + "Conditions: " + desc + "<br />" +  "Wind Chill " + calcWindChill.toFixed(0) + "(°F)";
-  }
-  // Display message in h3
-  document.getElementById('windChill').innerHTML = message;
-  
+        const currentTemp = document.getElementById('temp');
+        const currentSpeed = document.getElementById('windSpeed');
+        const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        const name = document.querySelector('figcaption')
 
-function windChill(temp, wind) {
-  // declare variables set them to values found in text box
-  let t = temp;
-  let w = wind;
-  // math
-  let chill =
-    35.75 +
-    0.6215 * t -
-    35.75 * Math.pow(w, 0.16) +
-    0.4275 * t * Math.pow(w, 0.16);
+        currentSpeed.textContent = jsObject.wind.speed * 0.62.toFixed(0);
+        currentTemp.textContent = jsObject.main.temp;
+        var aCurrentTemp = 1.8*(currentTemp-273) + 32;
+        var actualCurrentTemp = aCurrentTemp.toFixed(0);
+        let desc = jsObject.weather[0].description;
+        desc = desc.split(' ').map(capitalize).join(' ');
+
+
+        document.getElementById('weathericon').setAttribute('src', iconsrc);
+        document.getElementById('weathericon').setAttribute('alt', desc);
+        document.getElementById('figcaption').textContent = desc;
+        name.innerHTML = `<h3>Currently: ${desc}</h3>`;
+
+        var tempF = actualCurrentTemp.textContent;
+        var speed = currentSpeed.textContent;
+        var display = windChill(tempF, speed);
+
+        let windchill = "";
+        if (tempF <= 50 || speed > 3) {
+            windchill = windChill(tempF, speed);
+            windchill = `${windchill}°F`;
+        } else {
+            windchill = "N/A";
+        }
+
+        document.getElementById('chill').innerHTML = display;
+    });
+
+
+function windChill(tempF, speed) {
+    var T = tempF;
+    var S = speed;
+    var F = 35.74 + 0.6215 * T - 35.75 * Math.pow(S, 0.16) + 0.4275 * T * Math.pow(S, 0.16);
+    return F.toFixed(0);
+
 }
-  return chill;
-});
