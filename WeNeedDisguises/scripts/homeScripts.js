9fcba0da0a7486560fe42df530f18138e05302d7
BreKussell <rus17008@byui.edu>
2022-04-04 19:58:27 -0600

    final

---
+++ WeNeedDisguises/scripts/homeScripts.js
@@ -0,0 +1,70 @@
+// mobile navigation 
+function mobileNav() {
+  let x = document.getElementById("navigation");
+  if (x.className === "mobileNavigation") {
+    x.className += " responsive";
+  } else {
+    x.className = "mobileNavigation";
+  }
+}
+
+// slides
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+// Lazy Loading
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 1, 
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages =(image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) =>{
+          if(item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target);
+          }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
